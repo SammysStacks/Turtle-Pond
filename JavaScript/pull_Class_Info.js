418da94bd03c6bc16fb6be95c435276f8411b181
samuel Solomon <sasolomon1998@gmail.com>
2021-03-02 11:37:49 -0800

    Minor Edits

--- JavaScript/pull_Class_Info.js
+++ JavaScript/pull_Class_Info.js
@@ -744,8 +744,8 @@ $(document).ready(function(){
   // Change this number if you want to clear all stored cookies/cache after a major change to the database
   let refresh_Update_Number = 2;
   let refresh_First_Term = 2;
-  let refresh_Second_Term = 3;
-  let refresh_Third_Term = 2;
+  let refresh_Second_Term = 2;
+  let refresh_Third_Term = 6;
   if (window.pageState === null || window.pageState.Updated != refresh_Update_Number) {
     let current_Class_List = document.getElementById("display_Added_Classes");
     let current_Calendar_State = document.getElementById("calendar_Body");
